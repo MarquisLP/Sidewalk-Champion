4af9f51da1557715a1eaaac1c2828de4dfe5b7c7
Mark Padilla <markjjpadilla@gmail.com>
2015-01-08 20:57:25 -0500

    Add DEFAULT_NAMES as a global tuple constant
    They will be referenced in various places in the game and should not
    be subject to change.
    
    INPUT_NAMES was also changed into a tuple for guaranteed immutability.

--- lib/globals.py
+++ lib/globals.py
@@ -10,13 +10,38 @@ Module Constants:
                     updated each second. The game uses a universal
                     'frame' unit to measure time; it is equivalent
                     to (1/FRAME_RATE) seconds.
-    INPUT_NAMES     A list containing all of the names for the possible
+    INPUT_NAMES     A tuple containing all of the names for the possible
                     input 'buttons' in the game. Each one is bound to a
                     different key for each player.
+    DEFAULT_ACTIONS A tuple of Strings, containing the names of all
+                    Actions that every character should have, such as
+                    walking, blocking, and jumping.
 """
 SCREEN_SIZE = (384, 226)
 FULL_SCALE = 3
 FRAME_RATE = 60.0
-INPUT_NAMES = ["up", "back", "down", "forward", "light_punch",
+INPUT_NAMES = ("up", "back", "down", "forward", "light_punch",
                "medium_punch", "heavy_punch", "light_kick",
-               "medium_kick", "heavy_kick", "start", "cancel"]
+               "medium_kick", "heavy_kick", "start", "cancel")
+DEFAULT_NAMES = ('intro',
+                 'stand',
+                 'walk',
+                 'crouch_down',
+                 'crouching_idle',
+                 'jump_up',
+                 'jump_forward',
+                 'jump_back',
+                 'block_standing',
+                 'block_high',
+                 'block_low',
+                 'standing_recoil',
+                 'crouching_recoil',
+                 'jumping_recoil',
+                 'tripped',
+                 'launched',
+                 'falling',
+                 'knockdown',
+                 'recover',
+                 'dizzy',
+                 'chip_ko',
+                 'victory')
