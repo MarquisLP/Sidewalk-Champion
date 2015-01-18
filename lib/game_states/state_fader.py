941f4755d49fcaf2af309a7cc091e85ec9cd8cff
Mark Padilla <markjjpadilla@gmail.com>
2015-01-17 20:42:53 -0500

    StateFader: Use a time-based fade speed
    Instead of incrementing or decrementing the State Surface alpha by a
    fixed value in FADE_RATE, the fade could be made much smoother by
    multiplying a FADE_SPEED value by the amount of time elapsed and adding
    or subtracting that from the Surface alpha.
    
    This is because the game's frame rate is slightly inconsistent.

--- lib/game_states/state_fader.py
+++ lib/game_states/state_fader.py
@@ -5,8 +5,8 @@ class StateFader(object):
     game processing once the fade finishes.
 
     Class Constants:
-        FADE_RATE: An integer for the amount of alpha transparency gained
-            or lost by the State Surface during each update cycle.
+        FADE_SPEED: An integer for the speed of the fade, in alpha value
+            gained/lost per second.
 
     Attributes:
         next_state: An integer for the ID of the next Game State to go
@@ -20,7 +20,7 @@ class StateFader(object):
         is_running: A Boolean indicating whether the fade is still
             running.
     """
-    FADE_RATE = 7
+    FADE_SPEED = 450
 
     def __init__(self, change_state):
         """Declare and initialize instance variables.
@@ -58,12 +58,14 @@ class StateFader(object):
         self.next_state = next_state
         self.is_running = True
 
-    def fade_in_state(self, state_surf):
+    def fade_in_state(self, time, state_surf):
         """Fade in a Game State's Surface.
 
         The fade will end once the Surface is completely opaque.
 
         Args:
+            time: A float for the time elapsed, in seconds, since the
+                last update cycle.
             state_surf: A Surface containing all of a Game State's
                 graphical components.
         """
@@ -71,15 +73,17 @@ class StateFader(object):
         if old_alpha >= 255:
             self.is_running = False
         else:
-            state_surf.set_alpha(old_alpha + self.FADE_RATE)
+            state_surf.set_alpha(old_alpha + (self.FADE_SPEED * time))
 
-    def fade_out_state(self, state_surf):
+    def fade_out_state(self, time, state_surf):
         """Fade out a Game State's Surface.
 
         The fade will end once the Surface is completely transparent.
         Once this happens, the game will switch to the next Game State.
 
         Args:
+            time: A float for the time elapsed, in seconds, since the
+                last update cycle.
             state_surf: A Surface containing all of a Game State's
                 graphical components.
         """
@@ -88,4 +92,4 @@ class StateFader(object):
             self.is_running = False
             self.change_state(self.next_state)
         else:
-            state_surf.set_alpha(old_alpha - self.FADE_RATE)
+            state_surf.set_alpha(old_alpha - (self.FADE_SPEED * time))
