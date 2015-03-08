5782bbb5e483d4d976fa72902883db0c5fdc0e50
Mark Padilla <markjjpadilla@gmail.com>
2015-03-08 17:44:17 -0400

    Copy over get_character_paths()
    No changes need for this one.

--- lib/custom_data/character_loader_new.py
+++ lib/custom_data/character_loader_new.py
@@ -10,3 +10,12 @@ Attributes:
 """
 CHARACTER_LIST_PATH = 'characters/character_list.txt'
 FILEPATH_PREFIX = 'characters/'
+
+
+def get_character_paths():
+    """Return a list of all of the filepaths to the XML files for
+    playable characters.
+    """
+    with open(CHARACTER_LIST_PATH) as f:
+        character_path_list = [line.rstrip('\n') for line in f]
+        return character_path_list
