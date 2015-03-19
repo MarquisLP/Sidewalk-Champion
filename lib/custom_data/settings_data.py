5160aa8c9d045781bb84c5c43319468be4a4e89a
Mark Padilla <markjjpadilla@gmail.com>
2015-03-18 21:01:46 -0400

    Use default bindings when creating SettingsData
    This will make for less code in settings_manager.

--- lib/custom_data/settings_data.py
+++ lib/custom_data/settings_data.py
@@ -4,8 +4,8 @@ class SettingsData(object):
     
     Attributes
         screen_scale        The window and all Graphics will be
-                            magnified by this scale. This can either be
-                            1 or 2; any other value will default to 1.
+                            magnified by this scale. This can be either 1, 2,
+                            or 3; any other value will default to 1.
         show_box_display    Set to True if collision boxes are
                             displayed over the characters in battle.
         player1_keys        The key-binding Dictionary for player 1's
@@ -16,24 +16,41 @@ class SettingsData(object):
     def __init__(self):
         self.screen_scale = 1
         self.show_box_display = False
-        self.player1_keys = self.create_blank_keys_dict()
-        self.player2_keys = self.create_blank_keys_dict()
+        self.player1_keys = self.default_p1_keys_dict()
+        self.player2_keys = self.default_p2_keys_dict()
 
-    def create_blank_keys_dict(self):
-        """Return a Dictionary with all of the input names as keys
-        but with blank key-binding values.
+    def default_p1_keys_dict(self):
+        """Return a key input dict with the default bindings for player 1.
         """
-        bindings = {'up' : None,
-                    'back' : None,
-                    'down' : None,
-                    'forward' : None,
-                    'light_punch' : None,
-                    'medium_punch' : None,
-                    'heavy_punch' : None,
-                    'light_kick' : None,
-                    'medium_kick' : None,
-                    'heavy_kick' : None,
-                    'start' : None,
-                    'cancel' : None}
+        bindings = {'up' : 'w',
+                    'back' : 'a',
+                    'down' : 's',
+                    'forward' : 'd',
+                    'light_punch' : 'r',
+                    'medium_punch' : 't',
+                    'heavy_punch' : 'y',
+                    'light_kick' : 'f',
+                    'medium_kick' : 'g',
+                    'heavy_kick' : 'h',
+                    'start' : 'left ctrl',
+                    'cancel' : 'escape'}
+
+        return bindings
+
+    def default_p2_keys_dict(self):
+        """Return a key input dict with the default bindings for player 2.
+        """
+        bindings = {'up' : 'up',
+                    'back' : 'left',
+                    'down' : 'down',
+                    'forward' : 'right',
+                    'light_punch' : '[7]',
+                    'medium_punch' : '[8]',
+                    'heavy_punch' : '[9]',
+                    'light_kick' : '[4]',
+                    'medium_kick' : '[5]',
+                    'heavy_kick' : '[6]',
+                    'start' : 'return',
+                    'cancel' : 'backspace'}
 
         return bindings
