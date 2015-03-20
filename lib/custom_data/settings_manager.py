5d35b7df953e3db5fd28ce5d5311d80b8982f289
Mark Padilla <markjjpadilla@gmail.com>
2015-03-20 17:30:10 -0400

    Write load_settings()

--- lib/custom_data/settings_manager.py
+++ lib/custom_data/settings_manager.py
@@ -6,5 +6,19 @@ Attributes:
     SETTINGS_SCHEMA_PATH (String): The file path for the settings'
         XML Schema.
 """
+from lib.custom_data.xml_ops import load_xml_doc_as_object
+
+
 SETTINGS_PATH = 'settings.xml'
 SETTINGS_SCHEMA_PATH = 'settings.xsd'
+
+
+def load_settings():
+    """Load all Settings data from the settings file and return it as a
+    SettingsData object.
+
+    Returns:
+        A SettingsData object, or None if errors were encountered while
+        reading the settings file.
+    """
+    return load_xml_doc_as_object(SETTINGS_PATH, SETTINGS_SCHEMA_PATH)
