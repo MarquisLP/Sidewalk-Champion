28da54d17bbb6b3e9127c50387fe89c833c81f2a
Mark Padilla <markjjpadilla@gmail.com>
2015-08-02 14:19:44 -0400

    Re-add text_ops.py to master
    This file disappeared for some reason. It went unnoticed because the
    compiled Python file was still intact, somehow.

---
+++ lib/custom_data/text_ops.py
@@ -0,0 +1,29 @@
+"""This module provides methods for working with and performing various
+operations on text files.
+"""
+def get_prefixed_lines_from_txt(txt_path, prefix=''):
+    """Return a tuple containing all of the lines within a text file.
+    Each line can be prefixed with a specific String.
+
+    Args:
+        txt_path (String): The file path to a text file containing one
+            or more lines of text.
+        prefix (String): Optional. A String segment that will be
+            prepended to the start of each line.
+    """
+    with open(txt_path) as f:
+        lines = [prefix + line.rstrip('\n') for line in f]
+        return tuple(lines)
+
+
+def num_of_lines_in_txt(txt_path):
+    """Return the number of lines in a text file. None is returned if
+    the file path is invalid.
+
+    Args:
+        txt_path (String): The file path to a text file.
+    """
+    try:
+        return sum(1 for line in open(txt_path))
+    except:
+        return None
